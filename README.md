0f4f81f3ebeeedf8ef3f628c9d7251f5a05495ae
Brandon Blaylock <brandon@null.pub>
2019-03-16 17:15:57 -0700

    feat(generate): initial generate library

---
+++ README.md
@@ -0,0 +1,1 @@
+This directory contains the generated library. Any file landed here will likely be overwritten.
---
+++ generate.ts
@@ -0,0 +1,27 @@
+import { fromJSON, generate, serializeTypescript } from '@nll/swagger-codegen-ts';
+import { writeFileSync } from 'fs';
+import fetch from 'node-fetch';
+import { join } from 'path';
+
+const TEMP_FILE_NAME = join(__dirname, 'src', 'spec.json');
+
+const API_URL =
+  process.env.API_URL || 'http://prodapi.lodasoft.com/swagger/docs/v1';
+const LIB_PATH = join(__dirname, 'src');
+
+const makeClient = async (url: string, path: string) => {
+  const swagger = await fetch(url).then(r => r.text());
+  writeFileSync(TEMP_FILE_NAME, swagger);
+  return generate({
+    pathsToSpec: [TEMP_FILE_NAME],
+    out: path,
+    fileReader: fromJSON,
+    serialize: serializeTypescript,
+  });
+};
+
+const run = async () => {
+  await makeClient(API_URL, LIB_PATH);
+};
+
+run();
