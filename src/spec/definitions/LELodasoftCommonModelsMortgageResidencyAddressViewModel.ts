fa0e904dca973105929e35669a5e6d27c95f8182
Brandon Blaylock <brandon@null.pub>
2019-06-08 13:12:34 -0700

    fix(residencyBasis): added another unknown residency basis

--- src/spec/definitions/LELodasoftCommonModelsMortgageResidencyAddressViewModel.ts
+++ src/spec/definitions/LELodasoftCommonModelsMortgageResidencyAddressViewModel.ts
@@ -1,12 +1,12 @@
 import { createEnumType } from '@nll/utils-ts/lib/io';
 import { createOptionFromOptional } from '@nll/utils-ts/lib/io';
+import { DateFromDatelike } from '@nll/utils-ts/lib/io';
 import { Option } from 'fp-ts/lib/Option';
 import { boolean, number, string, type } from 'io-ts';
-import { DateFromDatelike } from '@nll/utils-ts/lib/io';
 
 import {
-	LELodasoftCommonModelsMortgageAddressViewModel,
-	LELodasoftCommonModelsMortgageAddressViewModelIO,
+  LELodasoftCommonModelsMortgageAddressViewModel,
+  LELodasoftCommonModelsMortgageAddressViewModelIO,
 } from '../definitions/LELodasoftCommonModelsMortgageAddressViewModel';
 
 export enum OwnRentTypeEnum {
@@ -20,6 +20,7 @@ export enum ResidencyBasisEnum {
 	'FormerAddress',
 	'MailingAddress',
 	'UnknownAddress',
+	'UnknownAddress2',
 }
 
 export type LELodasoftCommonModelsMortgageResidencyAddressViewModel = {
